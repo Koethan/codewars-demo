9b7aa5aa0db14d7d8e3913e6d8bbe457bc5458f6
Ethan Ospina <ethan_ospina@yahoo.com>
2022-11-29 19:31:27 -0500

    don't forget to make the whole string lowercase!

---
+++ js/solution019.js
@@ -0,0 +1,34 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+
+
+function XO(str) {
+    let x = 0
+    let o = 0
+    for(let i = 0; i < str.length; i++){
+      if(str.toLowerCase()[i] === 'x'){
+        x++ 
+      }else{
+        if(str.toLowerCase()[i] === 'o'){
+          o++
+        }
+      }
+    }
+  return x===o
+  }
+
+
+  console.log(XO("XXoOxO"))
+
+
+// function XO(str) {
+//     return str.toLowerCase().split('x').length === str.toLowerCase().split('o').length;
+// }
